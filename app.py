411020e7d9c61f608bf2e3aff30d598455ea4ee8
Avnish Ranjan <avnishranjan7@gmail.com>
2026-03-11 14:18:14 +0530

    fix bandit warning

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="templetes")
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
@@ -14,4 +14,4 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)  
