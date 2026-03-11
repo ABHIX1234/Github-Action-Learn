582441b1e95a594dea08a0c97ee8777858dda21d
Avnish Ranjan <avnishranjan7@gmail.com>
2026-03-11 14:31:07 +0530

    file change

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template
+app = Flask(__name__)
 
-app = Flask(__name__, template_folder="templates")
 
-
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/health")
+@app.route('/health')
 def health():
-    return "Server is up and running"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)  # nosec B104
+    return 'Server is up and running'
